57d3946c356542ff9669b2cb85a154ce2bb5fe4d
Simone Camporeale <simone.camporeale@netgrid.it>
2016-07-06 11:32:05 +0200

    Jersey 2.22 Guice 4 and Embedded Jetty

---
+++ src/main/java/it/netgrid/lovelace/Application.java
@@ -0,0 +1,24 @@
+package it.netgrid.lovelace;
+
+import org.glassfish.hk2.api.ServiceLocator;
+import org.glassfish.jersey.server.ResourceConfig;
+import org.jvnet.hk2.guice.bridge.api.GuiceBridge;
+import org.jvnet.hk2.guice.bridge.api.GuiceIntoHK2Bridge;
+
+import com.google.inject.Inject;
+
+public class Application extends ResourceConfig {
+
+	@Inject
+    public Application(ServiceLocator serviceLocator, GuiceConfig config) {
+        // Set package to look for resources in
+        packages(Main.RESOURCES_PACKAGES);
+
+        System.out.println("Registering injectables...");
+
+        GuiceBridge.getGuiceBridge().initializeGuiceBridge(serviceLocator);
+
+        GuiceIntoHK2Bridge guiceBridge = serviceLocator.getService(GuiceIntoHK2Bridge.class);
+        guiceBridge.bridgeGuiceInjector(config.getInjector());
+    }
+}
--- src/main/java/it/netgrid/lovelace/GuiceConfig.java
+++ src/main/java/it/netgrid/lovelace/GuiceConfig.java
@@ -1,19 +1,27 @@
 package it.netgrid.lovelace;
 
+import org.eclipse.jetty.util.log.Log;
+
 import com.google.inject.Injector;
 import com.google.inject.servlet.GuiceServletContextListener;
 
 public class GuiceConfig extends GuiceServletContextListener {
 	
-	private final Injector injector;
+    public static Injector injector;
 	
-	public GuiceConfig(Injector injector) {
-		this.injector = injector;
+	public GuiceConfig() {
+		if(injector!=null){
+			Log.getLog().debug("Injector already configured");
+		}
+	}
+	
+	public GuiceConfig(Injector _injector) {
+		injector = _injector;
 	}
 
 	@Override
 	protected Injector getInjector() {
-		return injector;
+		return Main.injector;
 	}
 
 }
